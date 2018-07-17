29f97af5a3e12fc6b7e15f19bb5fc1941b68ccd7
Daisy Leung <leungtszkuk@gmail.com>
2018-07-16 23:10:59 -0400

    add outer loop to loop through each ncut explicitly to work around yt bug

--- test_brute.py
+++ test_brute.py
@@ -45,72 +45,69 @@ if not os.path.isdir(outdir):
 
 field_select = "h2density"
 
-# th_list = 10**np.linspace(-0.5, 1.5, 7)
+th_list = 10**np.linspace(-0.5, 1.5, 7)
 # th_list = [7.]
 # th_list = [10**-0.5]
-test = False
+test = True
 
-# loop through all snapshots
-for snapshotnum in range(16, 29):
-    data = import_fetch_gal(isnap=snapshotnum)
+n_cell_min = 10
 
-    if test:
-        assert len(th_list) == 1
-        check_hist_h2(data, th_list, ss=snapshotnum, outdir=outdir)
-        f_out = outdir + "ss_" + str(snapshotnum) + "_ncut_" + "{:.2f}.png".format(th_list[0])
-    else:
-        f_out = outdir + "ss_" + str(snapshotnum) + "list_of_ncuts_{:.2f}-{:.2f}".format(th_list.min(), th_list.max()) + ".png"
+# because of the stupid yt bug, we will loop through the cuts and run clumpfinder one level at a time...
+for incut in th_list:
 
-    ds, dd = prepare_unigrid(data=data)
-    prj = yt.ProjectionPlot(ds, 0, field_select,
-                            center='c', weight_field='density')
+    # loop through all snapshots
+    for snapshotnum in range(16, 29):
+        data = import_fetch_gal(isnap=snapshotnum)
 
-    #prj.set_zlim('h2density', 1.e-3, 1e-1)
-    #th_list = np.linspace(0.01, .1, 3)
-    # th_list = [0.01]
+        check_hist_h2(data, incut, ss=snapshotnum, outdir=outdir)
+        f_out = outdir + "ss_" + str(snapshotnum) + "_ncut_" + "{:.2f}.png".format(incut)
 
-    for iii, nthres in enumerate(th_list):
-        cmax = None
-        _, leaf_clumps = ytclumpfind_H2(ds, dd, field_select, nthres,
-                                        c_max=cmax, step=1e+6,
-                                        N_cell_min=10, save=False,
-                                        plot=False, saveplot=None, fold_out='./')
-        # prj.annotate_clumps(leaf_clumps)       # this is a bug.. instead, loop
-        # through each leaf_clump an plot the contours..
+        ds, dd = prepare_unigrid(data=data)
+        prj = yt.ProjectionPlot(ds, 0, field_select,
+                                center='c', weight_field='density')
+
+        #prj.set_zlim('h2density', 1.e-3, 1e-1)
+        #th_list = np.linspace(0.01, .1, 3)
+        # th_list = [0.01]
+
+        _, leaf_clumps = ytclumpfind_H2(ds, dd, field_select, incut,
+                                            c_max=None, step=1e+6,
+                                            N_cell_min=n_cell_min, save=False,
+                                            plot=False, saveplot=None, fold_out='./')
 
         id_sorted = range(len(leaf_clumps))
         # leaf_clumps to sort by np.sum(leaf_clumps[id_sorted]["density"])
         id_sorted = sorted(range(len(leaf_clumps)),
-                           key=lambda x: np.sum(leaf_clumps[x]["density"]))
+                               key=lambda x: np.sum(leaf_clumps[x]["density"]))
 
         for ileaf in id_sorted:
             _fc = np.mean(leaf_clumps[ileaf].data.fcoords[:], axis=0)
 
-            if(1):
+            if(0):
                 ff = float(iii) / len(th_list)
             else:
                 ff = float(ileaf) / len(leaf_clumps)
 
             prj.annotate_clumps([leaf_clumps[ileaf]], plot_args={
-                                'colors': [col_f(ff)],
-                                'alpha': 0.8,
-                                'zorder': ileaf
-                                })
+                                    'colors': [col_f(ff)],
+                                    'alpha': 0.8,
+                                    'zorder': ileaf
+                                    })
 
             prj.annotate_marker(_fc,
-                                coord_system='data',
-                                plot_args={'color': 'red', 's': 500})
+                                    coord_system='data',
+                                    plot_args={'color': 'red', 's': 500})
             prj.annotate_text(_fc,
-                              ileaf,
-                              coord_system='data',
-                              text_args={'color': 'black', 'size': 8},
-                              inset_box_args={'boxstyle': 'square',
-                                              'facecolor': 'white',
-                                              'linewidth': 2.0,
-                                              'edgecolor': 'white',
-                                              'alpha': 0.35})
+                                  ileaf,
+                                  coord_system='data',
+                                  text_args={'color': 'black', 'size': 8},
+                                  inset_box_args={'boxstyle': 'square',
+                                                  'facecolor': 'white',
+                                                  'linewidth': 2.0,
+                                                  'edgecolor': 'white',
+                                                  'alpha': 0.35})
 
-        print 'ncut = ', nthres
+        print 'ncut = ', incut
         print '  N clumps', len(leaf_clumps)
         for i in xrange(len(leaf_clumps)):
             print '    id     ', i
@@ -118,12 +115,34 @@ for snapshotnum in range(16, 29):
             print '    max f*d', np.max(leaf_clumps[i]["h2density"])
             print '    min f*d', np.min(leaf_clumps[i]["h2density"])
 
-    prj.zoom(2)
-    prj.save(f_out)
-    print 'Dump to'
-    print '  ', f_out
-    print '\n'
+        prj.zoom(2)
+        prj.save(f_out)
+        print 'Dump to'
+        print '  ', f_out
+        print '\n'
 
-    prj = None
+        prj = None
+
+
+        from snapshot28_ytclump_fields import get_phyprop_of_leaf
+        import cPickle as pickle
+        print '  compute properties'
+        # to retreive physical properties of leaf
+        leaf_fields = {}
+        for n_leaf in range(len(leaf_clumps)):
+            leaf_fields[str(n_leaf)] = get_phyprop_of_leaf(leaf_clumps[n_leaf],
+                                                       data['density'],
+                                                       data['H2'] * data['density'],
+                                                       data['P'], data['P_nt'],
+                                                       data['Z'],
+                                                       data['velx'],
+                                                       data['vely'],
+                                                       data['velz'],
+                                                       plothist=False)
+        print "saved leaf fields: ", leaf_fields['0'].keys()
+        suboutdir = "leaf_fields_" + str(snapshotnum) + "/"
+
+        pickle.dump(leaf_fields, open(outdir + suboutdir + '{0:.2f}'.format(ncut) + '_' + str(n_cell_min) + "_fields.p", "wb"))
+
 
     # ------------
