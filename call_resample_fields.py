e358ec160227582e2b81eadcebbe8197d731ccfb
Daisy Leung <leungtszkuk@gmail.com>
2018-07-18 22:02:00 -0400

    clean up repo

--- call_resample_fields.py
+++ call_resample_fields.py
@@ -1,13 +1,13 @@
 '''
 
-Last mod: 13 July 2018
+Last mod: 18 July 2018
 
 
 '''
 
 import os
 import numpy as np
-from resample_fields import resam_each_field
+from gridding_modules.resample_fields import resam_each_field
 import pickle
 import h5py
 import matplotlib.pyplot as plt
@@ -17,7 +17,7 @@ plotdir = "resampled/"
 if not os.path.isdir(plotdir):
     os.mkdir(plotdir)
 
-folder = 'precomputed_data/'
+folder = '/mnt/home/daisyleung/mc_eor/precomputed_data/'
 f_camera = folder + 'camera_settings.log'
 with open(f_camera, 'rb') as f:
     data = pickle.load(f)
@@ -27,35 +27,35 @@ fieldsToExtract = ['rho', 'P_nt', 'P', 'H2', 'Z']
 
 for ssnum in snapshotToLoad:
 
-    # # saved in call_fetch_gal_fields.py
-    #  ds = np.load('snapshot' + str(ssnum) + '_center_fields0123456-15.npz')
+    # saved in call_fetch_gal_fields.py
+    ds = np.load('snapshot' + str(ssnum) + '_center_fields0123456-15.npz')
     outname = "snapshot" + str(ssnum) + "_center_fields0123456-15_resampled.h5"
 
-    #  if os.path.exists(outname):
-    #      os.system('rm ' + outname)
+    if os.path.exists(outname):
+        os.system('rm ' + outname)
 
-    #  originalSize = data[str(ssnum)]['size']
+    originalSize = data[str(ssnum)]['size']
 
-    #  #
-    #  dx_vector = ds['dx_vector']
-    #  loc_vector = ds['loc_vector']
+    #
+    dx_vector = ds['dx_vector']
+    loc_vector = ds['loc_vector']
 
-    #  fieldsDict = {}
-    #  for i in fieldsToExtract:
-    #      fieldsDict[i] = ds[i]
+    fieldsDict = {}
+    for i in fieldsToExtract:
+        fieldsDict[i] = ds[i]
 
-    #  for kkk, vvv in fieldsDict.iteritems():
-    #      resam_each_field(dx_vector, loc_vector, vvv, kkk, outname, originalSize, debug=False)
+    for kkk, vvv in fieldsDict.iteritems():
+        resam_each_field(dx_vector, loc_vector, vvv, kkk, outname, originalSize, debug=False)
 
-    #  # -- velocity --
-    #  axes = {'vel_x': ds['vel'][:, 0], 'vel_y': ds['vel'][:, 1], 'vel_z': ds['vel'][:, 2]}
+    # -- velocity --
+    axes = {'vel_x': ds['vel'][:, 0], 'vel_y': ds['vel'][:, 1], 'vel_z': ds['vel'][:, 2]}
 
-    #  for kkk, vvv in axes.iteritems():
-    #    resam_each_field(dx_vector, loc_vector, vvv, kkk, outname, originalSize, debug=False)
+    for kkk, vvv in axes.iteritems():
+        resam_each_field(dx_vector, loc_vector, vvv, kkk, outname, originalSize, debug=False)
 
     # ------ sanity check -- still in code units..
     f = h5py.File(outname, "r")
-    # careful sometimes i used "density" (e.g., resample.py), see
+    # careful sometimes i used "density", see
     # resample_fields.py to make sure
     H2density = f["rho"].value * f["H2"].value
     plt.figure()
