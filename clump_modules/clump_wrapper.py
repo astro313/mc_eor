cc4d833fcee8838db3f07492ae3c4543336f85ba
Daisy Leung <leungtszkuk@gmail.com>
2018-07-24 16:58:28 -0400

    fix bug

--- clump_modules/clump_wrapper.py
+++ clump_modules/clump_wrapper.py
@@ -208,13 +208,13 @@ def get_phyprop_of_leaf(subleaf, density, H2density, Pressure, P_nt, metallicity
 
     # print(subleaf.quantities.total_mass())
     # print(subleaf.quantities.center_of_mass())
-
+    
     for fff in _leaf_fields.iterkeys():
         if not fff in starPartDict.iterkeys():
             _leaf_fields[fff] = eval(fff)[ii, jj, kk]
         else:
-            _leaf_fields[fff] = starPartDict[kkk][ii, jj, kk]
-
+            _leaf_fields[fff] = starPartDict[fff][ii, jj, kk]
+        
     #_h2_subleaf = subleaf.data['h2density']
     #h2_subleaf = _leaf_fields['H2density']
     #assert round(_h2_subleaf[0]) == round(h2_subleaf[0])
