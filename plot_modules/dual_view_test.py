6a75d1ee9ff7a0d07a64c7593fea38ab67bf9278
Daisy Leung <leungtszkuk@gmail.com>
2018-07-24 17:12:27 -0400

    move plot dual into a function

--- plot_modules/dual_view_test.py
+++ plot_modules/dual_view_test.py
@@ -55,85 +55,84 @@ def calculate_eigenvektoren(data, sizekpc, selected_field='density', verbose = F
 
 
 
+def plot_face_edge(isnap=28, selected_field='density', sizekpc=7., cutLow=1.e-5, f_out=None):
 
-isnap = 28
-selected_field = 'density'
-sizekpc = 7.
+  if f_out is None:
+    f_out='test_'+selected_field+'_out'+str(isnap)+'_yt_unit_plot.png')
 
+  from mpl_toolkits.axes_grid import AxesGrid
+  import matplotlib.pyplot as plt
 
-from mpl_toolkits.axes_grid import AxesGrid
+  fig = plt.figure()
 
+  grid = AxesGrid(fig, (0.075,0.075,0.85,0.85),
+                  nrows_ncols = (1,2),
+                  axes_pad = 0.05,
+                  #label_mode = "L",
+                  label_mode = "1",
+                  share_all = True,
+                  cbar_location="right",
+                  cbar_mode="single",
+                  cbar_size="3%",
+                  cbar_pad= 0.01)
+  #grid = [fig.add_subplot(211),fig.add_subplot(212)]
 
-import matplotlib.pyplot as plt
+  data = import_fetch_gal(isnap=isnap)
 
-fig = plt.figure()
+  ds, dd = prepare_unigrid(data=data, 
+                           add_unit= True, 
+                           regionsize_kpc= sizekpc, 
+                           debug=False)
 
-grid = AxesGrid(fig, (0.075,0.075,0.85,0.85),
-                nrows_ncols = (1,2),
-                axes_pad = 0.05,
-                #label_mode = "L",
-                label_mode = "1",
-                share_all = True,
-                cbar_location="right",
-                cbar_mode="single",
-                cbar_size="3%",
-                cbar_pad= 0.01)
-#grid = [fig.add_subplot(211),fig.add_subplot(212)]
+  e_value,e_vectors = calculate_eigenvektoren(data=data,sizekpc=sizekpc)
 
-data = import_fetch_gal(isnap=isnap)
+  #los_vec = e_vectors[0,:] # face on
+  #los_vec = e_vectors[1,:] # edge on
 
-ds, dd = prepare_unigrid(data=data, 
-                         add_unit= True, 
-                         regionsize_kpc= sizekpc, 
-                         debug=False)
+  los_vec = e_vectors[2,:] # face on (again)
+  #up_vec  = e_vectors[1,:]
+  #up_vec  = np.cross(los_vec,up_vec)
 
-e_value,e_vectors = calculate_eigenvektoren(data=data,sizekpc=sizekpc)
+  vec_list = [ e_vectors[2,:],e_vectors[1,:]]
 
-#los_vec = e_vectors[0,:] # face on
-#los_vec = e_vectors[1,:] # edge on
+  for iplot,los_vec in zip(xrange(2),vec_list):
 
-los_vec = e_vectors[2,:] # face on (again)
-#up_vec  = e_vectors[1,:]
-#up_vec  = np.cross(los_vec,up_vec)
+    prj = yt.OffAxisProjectionPlot(ds = ds, center = [0,0,0], normal = los_vec , fields= selected_field
+      ,width       =(4, 'kpc')
+      #,north_vector=up_vec
+      ,weight_field='density'
+      )
 
-vec_list = [ e_vectors[2,:],e_vectors[1,:]]
+    prj.set_cmap(field=selected_field, cmap='inferno')
 
-for iplot,los_vec in zip(xrange(2),vec_list):
+    if selected_field == 'density':
+      selected_unit = 'Msun/pc**3'
+    elif selected_field == 'h2density':
+      selected_unit = '1/cm**3'
+    else:
+      raise TypeError('unit not implemented for the field')
+    prj.set_unit(selected_field, selected_unit)
+    prj.set_zlim(selected_field, cutLow * dd[selected_field].max().to(selected_unit), \
+                 dd[selected_field].max().to(selected_unit))
 
-  prj = yt.OffAxisProjectionPlot(ds = ds, center = [0,0,0], normal = los_vec , fields= selected_field
-    ,width       =(4, 'kpc')
-    #,north_vector=up_vec
-    ,weight_field='density'
-    )
+    plot        = prj.plots[selected_field]
+    plot.figure = fig
+    plot.axes   = grid[iplot].axes
+    plot.cax    = grid.cbar_axes[iplot]
 
-  prj.set_cmap(field=selected_field, cmap='inferno')
+    #prj.set_minorticks('all','off')
+    # Finally, this actually redraws the plot.
+    prj._setup_plots()
 
-  if selected_field == 'density':
-    selected_unit = 'Msun/pc**3'
-  else:
-    raise TypeError('unit not implemented for the field')
-  prj.set_unit(selected_field, selected_unit)
-  prj.set_zlim(selected_field, 1e-5 * dd[selected_field].max().to(selected_unit), dd[selected_field].max().to(selected_unit))
+  for cax in grid.cbar_axes:
+      cax.toggle_label(True)
+      #cax.axis[cax.orientation].set_label(field_select)
 
-  plot        = prj.plots[selected_field]
-  plot.figure = fig
-  plot.axes   = grid[iplot].axes
-  plot.cax    = grid.cbar_axes[iplot]
-
-  #prj.set_minorticks('all','off')
-  # Finally, this actually redraws the plot.
-  prj._setup_plots()
-
-for cax in grid.cbar_axes:
-    cax.toggle_label(True)
-    #cax.axis[cax.orientation].set_label(field_select)
-
-f_out = 'test_'+selected_field+'_out'+str(isnap)+'_yt_unit_plot.png'
-
-#prj.save(f_out)
-prj.save(f_out, mpl_kwargs={'bbox_inches':'tight'})
-print 'dump to ',f_out
+  prj.save(f_out, mpl_kwargs={'bbox_inches':'tight'})
+  print 'dump to ',f_out
 
 
 
+if __name__ == '__main__':
+  plot_face_edge()
 
