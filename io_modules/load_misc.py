a52f762b6b2055403f750c4e94a409ff36079eea
apallottini <andrea.pallottini85@gmail.com>
2018-08-13 15:55:18 +0200

    sfr path and added exception for single element case for sfr

--- io_modules/load_misc.py
+++ io_modules/load_misc.py
@@ -1,15 +1,22 @@
 import numpy as np
 import cPickle as pickle
 
-def load_SFR_perSS(filename="/mnt/home/daisyleung/mc_eor/star_particles/SFRs.txt"):
+import os
+here     = os.path.dirname(os.path.abspath(__file__))
+datapath = here[:here.rfind('/')]+'/star_particles/'
+
+def load_SFR_perSS(filename=datapath+"SFRs.txt"):
     """ load in globally-integrated SFR calculated for each snapshot, from a file saved before.
     """
     sfr = {}
 
     ss, sfr_4myr = np.loadtxt(filename, unpack=True, usecols=(0, 2))
 
-    for idx, iii in enumerate(ss):
+    try:
+      for idx, iii in enumerate(ss):
         sfr[str(int(iii))] = sfr_4myr[idx]
+    except TypeError:
+      sfr[str(int(ss))] = sfr_4myr
 
     return sfr
 
