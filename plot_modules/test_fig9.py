607995dc08b8cc93d1f38b623629710ac44b8f6f
Daisy Leung <leungtszkuk@gmail.com>
2018-09-07 20:57:45 -0400

    to reproduce Fig 9 of Pallo+17b

---
+++ plot_modules/test_fig9.py
@@ -0,0 +1,67 @@
+'''
+
+Reproduce Pallottini17b Fig. 9
+
+Last mod: 7 Sept 2018
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from plot_modules.plot_cloud_prop import setup_plot
+
+setup_plot()
+
+path = '../literature/data/'
+
+
+plt.close('all')
+plt.figure()
+# Althaea
+tff = 1.0
+plt.plot(np.log10(222.02/tff), np.log10(0.83), marker='*', markersize=15, color='red', label=r'Alth{\ae}a', linestyle='None', zorder=30)
+
+
+file_list = ['sk_kennicutt1998.dat', 'sk_bouche07a.dat',  'sk_daddi10b.dat',
+             'sk_daddi10a.dat', 'sk_tacconi10a.dat', 'sk_genzel10a.dat']
+label_list = ['Kennicutt+98', r"Bouch\'e+07",
+              r'Daddi+10', '', 'Tacconi+10', 'Genzel+10']
+list_2 = ['sk_Heiderman10.dat', 'sk_Lada10.dat']
+label_list_2 = ['Heiderman+10', 'Lada+10']
+
+colors = ('k', 'g', 'darkred', 'c', 'blue', 'orange')
+markers = ('.', 's', 'D', 'v', 'h', '8', '1')
+morecolors = ('darkolivegreen', 'brown')
+moremarkers = ('x', '+')
+
+for i in xrange(len(file_list)):
+
+    f_in = path + file_list[i]
+    ss = np.loadtxt(f_in, usecols=(2, 3, 4, 5, 6, 7, 8, 9))
+    x = ss[:, 6]
+    y = ss[:, 3]
+
+    plt.plot(x, y, label=label_list[i], marker=markers[i], linestyle='None', color=colors[i], markersize=5)
+
+
+for i in xrange(len(list_2)):
+    f_in = path + list_2[i]
+    ss = np.loadtxt(f_in, usecols=(5, 8))
+
+    x = np.log10(ss[:, 1])
+    y = np.log10(ss[:, 0])
+    plt.plot(x, y, marker=moremarkers[i], linestyle='None', label=label_list_2[i], color=morecolors[i], markersize=8)
+
+
+plt.legend(loc="best")
+
+from matplotlib.ticker import MultipleLocator
+ml = MultipleLocator(5)
+plt.axes().yaxis.set_minor_locator(ml)
+plt.axes().xaxis.set_minor_locator(ml)
+plt.minorticks_on()
+
+plt.ylabel(r"$\log {\left(\Sigma_{\rm SFR}\right)}$ [$M_{\odot}$ Myr$^{-1}$ pc$^{-2}$]")
+plt.xlabel(r"$\log {\left(\Sigma_{\rm gas}/t_{\rm ff}\right)}$ [$M_{\odot}$  pc$^{-2}$ Myr$^{-1}$]")
+plt.savefig('Test_PalloFig9.png', bbox_inches="tight")
+
