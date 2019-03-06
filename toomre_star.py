a14874986e612378682e9e539eec0d245afcbc7f
apallottini <andrea.pallottini85@gmail.com>
2019-03-06 11:56:15 +0100

    verbose level conditions added

--- toomre_star.py
+++ toomre_star.py
@@ -34,8 +34,11 @@ with open(f_camera, 'rb') as f:
     cameraDat = pickle.load(f)
 
 # prepare input data, if not already passed as arguments
-isnap = 16
+isnap = 28
 read_proper_unit = True
+
+verbose     = True
+megaverbose = False
 
 wg_var = 'mass'
 
@@ -53,8 +56,9 @@ else:
 # read in particles
 # read from resampled.h5
 starData = import_fetch_stars(
-    isnap=isnap, verbose=True, convert=True)   # default, convert=True
-print starData.keys()
+    isnap=isnap, verbose=verbose, convert=True)   # default, convert=True
+if verbose:
+  print starData.keys()
 
 ds, dd = prepare_star_unigrid(data=starData,
                               add_unit=True,     # since convert=True
@@ -76,7 +80,8 @@ yyy = dd["y"].convert_to_units('kpc').reshape((n_bins, n_bins, n_bins))
 zzz = dd["z"].convert_to_units('kpc').reshape((n_bins, n_bins, n_bins))
 center = dd.get_field_parameter('center')
 
-print dd['mass'].min()
+if verbose:
+  print 'max/min mass',dd['mass'].max(),dd['mass'].min()
 factor_mass, unit_mass = get_units(ro=ro)['mass']
 # mass_code_unit = dd['mass'].value/factor_mass
 
@@ -85,8 +90,12 @@ dx = abs(xxx[0][1:] - xxx[1][:-1]).min().convert_to_units('pc')
 dy = abs(yyy[0][1:] - yyy[1][:-1]).min().convert_to_units('pc')
 dz = np.diff(zzz[0][0]).min().convert_to_units('pc')
 rho_star = dd['mass'] / dx / dy / dz
-print 'rho_star in Msun/pc^3: ', rho_star
-print rho_star.min(), rho_star.max()
+if verbose:
+  print 'rho_star in Msun/pc^3: '
+  print '  max/min', rho_star.min(), rho_star.max()
+if megaverbose:
+  print rho_star
+
 # print np.sum(rho_star, axis=-1, dtype='float64') * dx
 
 # rho of star from mass
@@ -95,13 +104,15 @@ def _rho_star(field, data):
     rho = (data['mass'].convert_to_units('g')) / dx.convert_to_units('cm') / dy.convert_to_units('cm') / dz.convert_to_units('cm')
     return rho
 
-print dd['mass']
-print dx, dy, dz
+if megaverbose:
+  print dd['mass']
+  print dx, dy, dz
 
 ds.add_field(("rho_star"), function=_rho_star,
               units='g/cm**3')
 _dd = ds.all_data()
-print _dd['rho_star']
+if megaverbose:
+  print _dd['rho_star']
 
 for kk, vv in axes.iteritems():
 
@@ -162,7 +173,8 @@ projected_starSurfaceDensity_plane = projected_starSurfaceDensity[plane]
 starSD = projected_starSurfaceDensity_plane
 
 starSD = starSD.convert_to_units('Msun/pc**2')
-print 'max/min star SD in Msun/pc^2', np.max(starSD), np.min(starSD)
+if verbose:
+  print 'max/min star SD in Msun/pc^2', np.max(starSD), np.min(starSD)
 
 
 plt.figure()
@@ -178,7 +190,8 @@ plt.show(block=False)
 i_hat = coords_plane[0] - center_plane[plane][0]
 j_hat = coords_plane[1] - center_plane[plane][1]
 R = np.sqrt(i_hat**2 + j_hat**2)    # + k_hat**2)
-print R.min()
+if verbose:
+  print R.min()
 i_hat /= R
 j_hat /= R
 #
@@ -186,14 +199,16 @@ vi = vel_plane[0]
 vj = vel_plane[1]
 radial_vel = (vi * i_hat + vj * j_hat)
 radial_veloDisp = np.std(radial_vel)
-print 'radial velocity           ', np.max(radial_vel), np.min(radial_vel)
-print 'radial velocity dispersion', radial_veloDisp
+if verbose:
+  print 'radial velocity           ', np.max(radial_vel), np.min(radial_vel)
+  print 'radial velocity dispersion', radial_veloDisp
 
 # P_nt and P contribute to the velocity field in any case, as their divergence are sources in the Euler eq.
 i_hat_3d = coords3d_plane[0] - center_plane[plane][0]
 j_hat_3d = coords3d_plane[1] - center_plane[plane][1]
 R_3d = np.sqrt(i_hat_3d**2 + j_hat_3d**2)
-print R_3d.min()
+if megaverbose:
+  print R_3d.min()
 i_hat_3d /= R_3d
 j_hat_3d /= R_3d
 
@@ -215,15 +230,17 @@ plt.imshow(np.log10(sigma_r.value), cmap=cmap, origin='lower')
 plt.title(r'$\sigma_r$ weighted by ' + wg_var)
 plt.colorbar()
 plt.show(block=False)
-print 'max/min radial vdisp in km/s (from velocity)', np.max(sigma_r), np.min(sigma_r)
+if verbose:
+  print 'max/min radial vdisp in km/s (from velocity)', np.max(sigma_r), np.min(sigma_r)
 
 
 # v_phi
 theta = np.arctan2(j_hat, i_hat)
 _v_r = np.cos(theta) * vi + np.sin(theta) * vj
 v_phi = (-np.sin(theta) * vi + np.cos(theta) * vj)
-print 'maxmin v_phi    ', np.max(v_phi), np.min(v_phi)
-print 'std v_phi', np.std(v_phi)    # km/s
+if verbose:
+  print 'maxmin v_phi    ', np.max(v_phi), np.min(v_phi)
+  print 'std v_phi', np.std(v_phi)    # km/s
 
 # kappa
 R_cm = R.value * 1.e3 * pc2cm
@@ -245,9 +262,11 @@ r_slice = np.sqrt(x_slice**2 + y_slice**2)    # cm
 
 omega_measured = v_phi / r_slice
 omega_measured_standard_unit = v_phi / 1.e5 / (r_slice / pc2cm / 1.e3)
-print(omega_measured_standard_unit).mean()      # km/s/kpc
 omega_mw_kms_kpc = 220. / 8
-print omega_mw_kms_kpc
+if verbose:
+  print 'omega [km/s/kpc]'
+  print '  mean',(omega_measured_standard_unit).mean()      # km/s/kpc
+  print '  MW  ',omega_mw_kms_kpc
 
 plt.figure()
 plt.imshow(np.log10(omega_measured))
@@ -264,7 +283,8 @@ bin_centers = bins[:-1] + (bins[1:] - bins[:-1]) / 2.
 # Count how many pixels fall into each radial bin
 hist, _ = np.histogram(r_slice, bins)
 hist[hist == 0] = 1
-print hist
+if megaverbose:
+  print hist
 
 # Get flat 1D indices into r_slice for each bin.
 inds = np.digitize(r_slice.flat, bins) - 1
@@ -310,7 +330,10 @@ kappa_sq = 2 * rotation_frequency / r_slice * (
     2 * r_slice * rotation_frequency + r_slice**2 * domega_dr)
 kappa_sq[kappa_sq < 0] = np.min(kappa_sq[kappa_sq > 0])
 kappa = np.sqrt(kappa_sq)
-print kappa      # in the MW, kappa ~ omega, which is also true here
+if verbose:
+  print 'max min kappa',np.max(kappa),np.min(kappa)
+if megaverbose:
+  print kappa      # in the MW, kappa ~ omega, which is also true here
 
 plt.figure()
 plt.imshow(np.log10(kappa), cmap=cmap, origin='lower')
@@ -322,7 +345,11 @@ A_star = 3.36
 G = 6.67259e-8  # cgs
 
 SD = starSD.convert_to_cgs()
-print 'starSD: ', starSD
+if verbose:
+  print 'starSD: '
+  print '  max/min',np.max(SD),np.min(SD)
+if megaverbose:
+  print starSD
 radial_veloDisp_cgs = sigma_r * 1.e5
 whnzero = np.where(SD.value != 0)[0]
 whnzero = whnzero.reshape((-1, int(np.sqrt(len(whnzero)))))
@@ -330,8 +357,12 @@ Q_star = np.zeros(SD.shape) * np.nan
 Q_star[whnzero] = radial_veloDisp_cgs * kappa[whnzero] / \
     (A_star * G * SD[whnzero].value)
 
-print "Q_star: ", Q_star
-print(np.isnan(Q_star) == True).any()
+if verbose:
+  print "Q_star: "
+  print '  max/min',np.max(Q_star),np.min(Q_star)
+if megaverbose:
+  print Q_star
+  print(np.isnan(Q_star) == True).any()
 
 # define plot boundary kpc
 _xmin =  (coords_plane[0] - center_plane[plane][0]).min()
@@ -455,7 +486,12 @@ cbar.set_label(r"$\log{Q_{\rm star}}$", fontsize=16)
 
 # plt.tight_layout()
 plt.show(block=False)
-# plt.savefig('ss_' + str(isnap) + '_toomre_proj_' + plane + '.png')
+
+f_out = 'ss_' + str(isnap) + 'stars_toomre_proj_' + plane + '.png'
+if verbose:
+  print 'out to'
+  print '  ',f_out
+plt.savefig(f_out)
 
 
 # #from scipy import interpolate
