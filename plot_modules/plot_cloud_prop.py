59a66029c9f64ea6b3b62f559d0afc72fd572323
Daisy Leung <leungtszkuk@gmail.com>
2019-03-11 16:58:42 -0400

    fontsize stuff

--- plot_modules/plot_cloud_prop.py
+++ plot_modules/plot_cloud_prop.py
@@ -16,21 +16,22 @@ def setup_plot():
     # print(matplotlib.matplotlib_fname())
 
     matplotlib.rcParams.update({'figure.figsize': (8, 5)    # inches
-                                , 'font.size': 12      # points
+                                , 'font.size': 16      # points
                                 , 'legend.fontsize': 10      # points
                                 , 'lines.linewidth': 2       # points
                                 , 'axes.linewidth': 1       # points
                                 , 'axes.prop_cycle': cycler('color', 'bgrcmyk')
                                 , 'text.usetex': True    # Use LaTeX to layout text
                                 , 'font.family': "serif"  # Use serifed fonts
-                                , 'xtick.major.size': 10     # length, points
+                                , 'xtick.major.size': 13     # length, points
                                 , 'xtick.major.width': 1     # points
-                                , 'xtick.minor.size': 6     # length, points
+                                , 'xtick.minor.size': 8     # length, points
                                 , 'xtick.minor.width': 1     # points
-                                , 'ytick.major.size': 10     # length, points
+                                , 'ytick.major.size': 13     # length, points
                                 , 'ytick.major.width': 1     # points
-                                , 'ytick.minor.size': 6     # length, points
-
+                                , 'ytick.minor.size': 8     # length, points
+                                , 'xtick.labelsize': 14
+                                , 'ytick.labelsize': 14
                                 , 'ytick.minor.width': 1     # points
                                 , 'font.serif': ("Times", "Palatino", "Computer Modern Roman", "New Century Schoolbook", "Bookman"), 'font.sans-serif': ("Helvetica", "Avant Garde", "Computer Modern Sans serif"), 'font.monospace': ("Courier", "Computer Modern Typewriter"), 'font.cursive': "Zapf Chancery"
                                 })
